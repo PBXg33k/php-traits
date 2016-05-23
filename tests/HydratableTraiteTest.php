04bd46e96439a69db82868626b754e27420e2231
Oguzhan Uysal <development@oguzhanuysal.eu>
2016-05-24 01:56:03 +0200

    Added tests

---
+++ tests/HydratableTraiteTest.php
@@ -0,0 +1,75 @@
+<?php
+require_once('stubs/HydratableTraitStub.php');
+
+/**
+ * Created by PhpStorm.
+ * User: PBX_g33k
+ * Date: 24-May-16
+ * Time: 00:31
+ */
+class HydratableTraiteTest extends PHPUnit_Framework_TestCase
+{
+    const VAL_PUBLIC_PROPERTY       = 'public propety value';
+    const VAL_PROTECTED_PROPERTY    = 'protected property value';
+    const VAL_PRIVATE_PROPERTY      = 'private property value';
+    const VAL_STRING_PROPERTY       = 'string';
+    const VAL_FLOAT_PROPERTY        = 3.9;
+    const VAL_INTEGER_PROPERTY      = 39;
+    const VAL_BOOLEAN_PROPERTY      = true;
+    const VAL_NULL_PROPERTY         = null;
+    const VAL_ARRAY_PROPERTY        = [ 0 => 'zero', 1 => 1, 'b', 'c' => 39];
+    const VAL_SNAKE_PROPERTY        = 'snake_case_property';
+    const VAL_DATETIME_PROPERTY     = '2016/05/24T01:03:00';
+
+    /**
+     * @var HydratableTraitStub
+     */
+    protected $testClass;
+
+    protected $dateTimeClass;
+    
+    protected $seed = [
+        'publicProperty'            => self::VAL_PUBLIC_PROPERTY,
+        'protectedProperty'         => self::VAL_PROTECTED_PROPERTY,
+        'privateProperty'           => self::VAL_PRIVATE_PROPERTY,
+        'stringProperty'            => self::VAL_STRING_PROPERTY,
+        'floatProperty'             => self::VAL_FLOAT_PROPERTY,
+        'booleanProperty'           => self::VAL_BOOLEAN_PROPERTY,
+        'nullProperty'              => self::VAL_NULL_PROPERTY,
+        'arrayProperty'             => self::VAL_ARRAY_PROPERTY,
+        'classProperty'             => 'initialize_me',
+        'dateTimeProperty'          => self::VAL_DATETIME_PROPERTY,
+        'snake_case_property'       => self::VAL_SNAKE_PROPERTY,
+    ];
+
+    public function setUp()
+    {
+        $this->testClass = new HydratableTraitStub;
+        $this->seed['classProperty'] = new \stdClass();
+        
+        $this->testClass->hydrateClass($this->seed);
+    }
+
+    public function testPublicProperty()
+    {
+        $this->assertEquals(self::VAL_PUBLIC_PROPERTY, $this->testClass->getPublicProperty());
+    }
+
+    public function testProtectedProperty()
+    {
+        $this->assertEquals(self::VAL_PROTECTED_PROPERTY, $this->testClass->getProtectedProperty());
+    }
+
+    public function testPrivateProperty()
+    {
+        $this->assertEquals(self::VAL_PRIVATE_PROPERTY, $this->testClass->getPrivateProperty());
+    }
+
+    public function testDateTimeProperty()
+    {
+        $this->assertInstanceOf(\DateTime::class, $this->testClass->getDateTimeProperty());
+        $this->assertEquals('2016', $this->testClass->getDateTimeProperty()->format('Y'));
+        $this->assertEquals('05', $this->testClass->getDateTimeProperty()->format('m'));
+        $this->assertEquals('24', $this->testClass->getDateTimeProperty()->format('d'));
+    }
+}
---
+++ tests/stubs/HydratableTraitStub.php
@@ -0,0 +1,301 @@
+<?php
+require_once(__DIR__.'/../../src/HydratableTrait.php');
+
+/**
+ * Created by PhpStorm.
+ * User: PBX_g33k
+ * Date: 24-May-16
+ * Time: 00:55
+ */
+class HydratableTraitStub
+{
+    use \Pbxg33k\Traits\HydratableTrait;
+
+    /**
+     * @var mixed
+     */
+    public $publicProperty;
+
+    /**
+     * @var mixed
+     */
+    protected $protectedProperty;
+
+    /**
+     * @var mixed
+     */
+    private $privateProperty;
+
+    /**
+     * @var string
+     */
+    private $stringProperty;
+
+    /**
+     * @var float
+     */
+    private $floatProperty;
+
+    /**
+     * @var boolean
+     */
+    private $booleanProperty;
+
+    /**
+     * @var null
+     */
+    private $nullProperty;
+
+    /**
+     * @var array
+     */
+    private $arrayProperty;
+
+    /**
+     * @var stdClass
+     */
+    private $classProperty;
+
+    /**
+     * @var DateTimeInterface
+     */
+    private $dateTimeInterfaceProperty;
+
+    /**
+     * @var DateTime
+     */
+    private $dateTimeProperty;
+
+    /**
+     * @var mixed
+     */
+    private $snake_case_property;
+
+    /**
+     * @return mixed
+     */
+    public function getPublicProperty()
+    {
+        return $this->publicProperty;
+    }
+
+    /**
+     * @param mixed $publicProperty
+     * @return HydratableTraitStub
+     */
+    public function setPublicProperty($publicProperty)
+    {
+        $this->publicProperty = $publicProperty;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProtectedProperty()
+    {
+        return $this->protectedProperty;
+    }
+
+    /**
+     * @param mixed $protectedProperty
+     * @return HydratableTraitStub
+     */
+    public function setProtectedProperty($protectedProperty)
+    {
+        $this->protectedProperty = $protectedProperty;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrivateProperty()
+    {
+        return $this->privateProperty;
+    }
+
+    /**
+     * @param mixed $privateProperty
+     * @return HydratableTraitStub
+     */
+    public function setPrivateProperty($privateProperty)
+    {
+        $this->privateProperty = $privateProperty;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStringProperty()
+    {
+        return $this->stringProperty;
+    }
+
+    /**
+     * @param string $stringProperty
+     * @return HydratableTraitStub
+     */
+    public function setStringProperty($stringProperty)
+    {
+        $this->stringProperty = $stringProperty;
+
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getFloatProperty()
+    {
+        return $this->floatProperty;
+    }
+
+    /**
+     * @param float $floatProperty
+     * @return HydratableTraitStub
+     */
+    public function setFloatProperty($floatProperty)
+    {
+        $this->floatProperty = $floatProperty;
+
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isBooleanProperty()
+    {
+        return $this->booleanProperty;
+    }
+
+    /**
+     * @param boolean $booleanProperty
+     * @return HydratableTraitStub
+     */
+    public function setBooleanProperty($booleanProperty)
+    {
+        $this->booleanProperty = $booleanProperty;
+
+        return $this;
+    }
+
+    /**
+     * @return null
+     */
+    public function getNullProperty()
+    {
+        return $this->nullProperty;
+    }
+
+    /**
+     * @param null $nullProperty
+     * @return HydratableTraitStub
+     */
+    public function setNullProperty($nullProperty)
+    {
+        $this->nullProperty = $nullProperty;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getArrayProperty()
+    {
+        return $this->arrayProperty;
+    }
+
+    /**
+     * @param array $arrayProperty
+     * @return HydratableTraitStub
+     */
+    public function setArrayProperty($arrayProperty)
+    {
+        $this->arrayProperty = $arrayProperty;
+
+        return $this;
+    }
+
+    /**
+     * @return stdClass
+     */
+    public function getClassProperty()
+    {
+        return $this->classProperty;
+    }
+
+    /**
+     * @param stdClass $classProperty
+     * @return HydratableTraitStub
+     */
+    public function setClassProperty($classProperty)
+    {
+        $this->classProperty = $classProperty;
+
+        return $this;
+    }
+
+    /**
+     * @return DateTimeInterface
+     */
+    public function getDateTimeInterfaceProperty()
+    {
+        return $this->dateTimeInterfaceProperty;
+    }
+
+    /**
+     * @param DateTimeInterface $dateTimeInterfaceProperty
+     * @return HydratableTraitStub
+     */
+    public function setDateTimeInterfaceProperty($dateTimeInterfaceProperty)
+    {
+        $this->dateTimeInterfaceProperty = $dateTimeInterfaceProperty;
+
+        return $this;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getDateTimeProperty()
+    {
+        return $this->dateTimeProperty;
+    }
+
+    /**
+     * @param DateTime $dateTimeProperty
+     * @return HydratableTraitStub
+     */
+    public function setDateTimeProperty($dateTimeProperty)
+    {
+        $this->dateTimeProperty = $dateTimeProperty;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSnakeCaseProperty()
+    {
+        return $this->snake_case_property;
+    }
+
+    /**
+     * @param mixed $snake_case_property
+     * @return HydratableTraitStub
+     */
+    public function setSnakeCaseProperty($snake_case_property)
+    {
+        $this->snake_case_property = $snake_case_property;
+
+        return $this;
+    }
+}
