adafdf6d4d5d81fd01edef947f09213006eccc6e
Oguzhan Uysal <development@oguzhanuysal.eu>
2016-05-24 16:57:32 +0200

    Refactored HydratableTrait, splitted logic

--- src/HydratableTrait.php
+++ src/HydratableTrait.php
@@ -52,6 +52,7 @@ trait HydratableTrait
      * If the value is an object, it initializes the object and assignes the initialized object.
      *
      * @param  object|array $class class
+     *
      * @return object
      */
     public function hydrateClass($class)
@@ -63,69 +64,110 @@ trait HydratableTrait
             // Check if key exists in $this
 
             // Convert key to a propertyname in $this
-            $propertyName = lcfirst(str_replace(' ', '', ucwords(str_replace('_', ' ', $itemKey))));
-            $propertyName = (property_exists($this, $propertyName)) ? $propertyName :
-                (property_exists($this, lcfirst($propertyName)) ? lcfirst($propertyName) :
-                    preg_replace_callback('/([A-Z])/', function ($match) {
-                        return strtolower('_' . $match[1]);
-                    }, lcfirst($propertyName))
-                );
-
-            // Check if property exists and assign a ReflectionProperty class to $reflectionProperty
-            if (
-                property_exists($this, $propertyName)
-                && $reflectionProperty = $reflection->getProperty($propertyName)
-            ) {
-                // Get the expected property class from the property's DocBlock
-                if ($propertyClassName = ReflectionTrait::getClassFromDocComment($reflectionProperty->getDocComment(), true,
-                    $reflection)
-                ) {
-
-                    // Set argument for constructor (if any), in case we're dealing with an object (IE: DateTime)
-                    $this->objectConstructorArguments = (in_array($propertyClassName, $this->giveDataInConstructor))
-                        ? $itemValue : null;
-
-                    if (in_array($propertyClassName, self::$nonObjectTypes)) {
-                        $this->setPropertyValue($propertyName, $itemValue, true);
-                    } elseif (interface_exists($propertyClassName)) {
-                        // We cannot instantiate an interface, so we skip it
-                        continue;
-                    } else {
-                        $object = new $propertyClassName($this->objectConstructorArguments);
-
-                        // Check if $object has valid values
-                        // IE: DateTime with a negative timestamp will cause a SQL Error
-                        $this->checkObjectForErrors($object, true);
-
-                        if ($object) {
-                            if (
-                                method_exists($object, 'hydrateClass') &&
-                                (is_array($itemValue) || is_object($itemValue))
-                            ) {
-                                $object->fromClass($itemValue);
-                            }
-                            // We're done. Assign the result to the propery of $this
-                            $this->setPropertyValue($propertyName, $object, true);
-                        }
-                    }
-                    unset($object);
-                }
-            }
+            $this->hydrateProperty($itemKey, $itemValue, $reflection);
         }
 
         return $this;
     }
 
     /**
+     * Hydrates property with value.
+     * Value can be anything. If the property is supposed to be a Class of anykind we will try to instantiate it
+     * and assign the class to the property
+     *
+     * @param                  $key
+     * @param                  $value
+     * @param \ReflectionClass $reflection
+     *
+     * @throws \Exception
+     */
+    protected function hydrateProperty($key, $value, \ReflectionClass $reflection)
+    {
+        $propertyName = $this->resolvePropertyName($key);
+
+        // Check if property exists and assign a ReflectionProperty class to $reflectionProperty
+        if (property_exists($this, $propertyName) && $reflectionProperty = $reflection->getProperty($propertyName)) {
+            // Get the expected property class from the property's DocBlock
+            if ($propertyClassName = ReflectionTrait::getClassFromDocComment($reflectionProperty->getDocComment(), true, $reflection)) {
+                // Set argument for constructor (if any), in case we're dealing with an object (IE: DateTime)
+                $this->objectConstructorArguments = (in_array($propertyClassName, $this->giveDataInConstructor)) ? $value : null;
+
+                if (!$this->isInstantiatable($propertyClassName)) {
+                    return;
+                }
+
+                if (!in_array($propertyClassName, self::$nonObjectTypes)) {
+                    $value = new $propertyClassName($this->objectConstructorArguments);
+                    $this->checkObjectForErrors($value, true);
+
+                    if (method_exists($value, 'hydrateClass') && $this->isHydratableValue($value)) {
+                        $value->hydrateClass($value);
+                    }
+                }
+
+                $this->setPropertyValue($propertyName, $value, true);
+            }
+        }
+    }
+
+    /**
+     * Resolves and returns propertyname
+     *
+     * @param $key
+     *
+     * @return mixed|string
+     */
+    private function resolvePropertyName($key)
+    {
+        $propertyName = lcfirst(str_replace(' ', '', ucwords(str_replace('_', ' ', $key))));
+
+        return (property_exists($this, $propertyName)) ? $propertyName :
+            (property_exists($this, lcfirst($propertyName)) ? lcfirst($propertyName) :
+                preg_replace_callback('/([A-Z])/', function($match) {
+                    return strtolower('_' . $match[1]);
+                }, lcfirst($propertyName))
+            );
+    }
+
+    /**
+     * Checks if the value can be hydrated for iteration
+     *
+     * @param $value
+     *
+     * @return bool
+     */
+    private function isHydratableValue($value)
+    {
+        return (is_array($value) || is_object($value));
+    }
+
+    /**
+     * Checks if Classname resolved from docblock can be instantiated
+     *
+     * @param $className
+     *
+     * @return bool
+     */
+    private function isInstantiatable($className)
+    {
+        return (!interface_exists($className));
+    }
+
+    /**
      * Checks (and fixes) objects against known errors
      *
      * @param Object &$object
-     * @param bool $fix Fix errors
+     * @param bool   $fix Fix errors
+     *
      * @return void
+     *
+     * @throws \Exception
      */
     private function checkObjectForErrors(&$object, $fix = false)
     {
-        if ($object instanceof \DateTime) {
+        if (!is_object($object)) {
+            throw new \Exception('Non-object passed');
+        } elseif ($object instanceof \DateTime) {
             // The constructor (passed from the API) is NULL, indicating an empty value
             // PHP DateTime's default value is now()
             if ($this->objectConstructorArguments == null) {
